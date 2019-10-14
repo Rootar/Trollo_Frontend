e2eb6f07b40f30d535e53888e19292cd0f88b002
Maciej Nabialczyk 209404@edu.p.lodz.pl <vxd55555@gmail.com>
2019-10-14 21:55:29 +0200

    Change index -> login

---
+++ trollo/trollo/src/login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './login.css';
+
+class SignIn extends React.Component
+{
+	render()
+	{
+		return (
+			<div className="signIn">
+				
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(
+	<SignIn/>,
+	document.getElementById('root')
+);
