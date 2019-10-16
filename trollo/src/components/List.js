6a2911e0022d0fb291469c8b21c46553c27dbf56
Rootar1 <mokrosinski95@gmail.com>
2019-10-16 08:15:49 +0200

    Borderplate app with react redux materialUI

---
+++ trollo/src/components/List.js
@@ -0,0 +1,37 @@
+import React from "react"
+
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+
+const Stylesx = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+      },
+      paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+      },
+}));
+
+const List = () => {
+    const Styles = Stylesx();
+
+    return (
+        <Grid container direction="column" className={Styles.root} xs={4}>
+            <Grid item>
+                <Paper className={Styles.paper}>"Treaksdjasusahudsahuashuasd"</Paper>
+            </Grid>
+            <Grid item>
+                <Paper className={Styles.paper}>"asgaiuuia"</Paper>                
+            </Grid>
+            <Grid item>
+                <Paper className={Styles.paper}>"sdasdagassshuasd"</Paper>                
+            </Grid>
+        </Grid>
+    )
+}
+
+export default List;
