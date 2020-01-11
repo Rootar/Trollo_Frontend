2bfec24b4eadb6454a01e581dcca980673377c5e
Maciej Nabialczyk 209404@edu.p.lodz.pl <vxd55555@gmail.com>
2020-01-11 12:42:44 +0100

    Work on change borad name.

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -7,10 +7,46 @@ import TextField from '@material-ui/core/TextField';
 import axios from 'axios';
 import PropTypes from 'prop-types'
 import {NotificationManager} from 'react-notifications';
+import EditIcon from '@material-ui/icons/Edit';
+import Popup from "reactjs-popup";
 
 const styles = theme => ({
     text:{
         marginTop: "20px"
+    },
+    editicon:{
+        marginTop: "20px",
+        marginLeft: "10px"
+    },
+    modal:{
+        size: "12px"
+    },
+    header:{
+        width: "100%",
+        size: "18px",
+        align: "center",
+        padding: "5px",
+    },
+    content:{
+        width: "100%",
+        padding: "10px 5px"
+    },
+    actions:{
+        width: "100%",
+        padding: "10px 5px",
+        margin: "auto",
+        align: "center"
+    },
+    close:{
+        cursor: "pointer",
+        position: "absolute",
+        display: "block",
+        padding: "2px 5px",
+        right: "-10px",
+        top: "-10px",
+        size: "24px",
+        background: "#ffffff",
+        border: "1px solid #cfcece",
     }
 });
 
@@ -20,11 +56,14 @@ class MainPage extends Component {
 
 		this.onChange = this.onChange.bind(this);
         this.onSubmitCreateBoard = this.onSubmitCreateBoard.bind(this);
+        this.setBoardName = this.setBoardName.bind(this);
     }
     
     state = {
         boardName: '',
-        boards: []
+        boards: [],
+        newBoardName: '',
+        newBoardId: 0
     }
 
     componentDidMount() {
@@ -65,12 +104,46 @@ class MainPage extends Component {
     }
 
     whenLoggedIn(){
-        // const {classes} = this.props
+        const {classes} = this.props
 
         return (
             <Grid container direction="column" justify="center" alignItems="center">
                 {this.state.boards.map((board) => (
-                    <Grid item><div>{board.name}</div><Button variant="contained" color="primary" onClick={() => this.onSubmitGoToBoard(board.boardId)}>Go to board</Button></Grid>                    
+                    <Grid item><div>{board.name}</div><Button variant="contained" color="primary" onClick={() => this.onSubmitGoToBoard(board.boardId)}>Go to board</Button>
+                            <Popup
+                                trigger={<button className="button"> <EditIcon/> </button>}
+                                modal>
+                                {close => (
+                                    <div className={classes.modal}>
+                                        <a className={classes.close} onClick={close}>
+                                            &times;
+                                        </a>
+                                        <div className={classes.header}> {board.name} </div>
+                                            <div className={classes.content}>
+                                                {" "}
+                                                <input onChange={ this.setBoardName } type="text" placeholder="type board name... " />
+                                            </div>
+                                            <div className={classes.actions}>
+                                                <button
+                                                    className="button"
+                                                    onClick={() => {
+                                                        this.onSetBoardName(board.boardId);
+                                                        close();
+                                                    }}>
+                                                    Save
+                                                </button>
+                                                <button
+                                                    className="button"
+                                                    onClick={() => {
+                                                        close();
+                                                    }}>
+                                                    Cancel
+                                                </button>
+                                        </div>
+                                    </div>
+                                )}
+                            </Popup>
+                    </Grid>                    
                 ))}      
 
                 <Grid direction="row" item container justify="center" alignItems="center">
@@ -81,8 +154,13 @@ class MainPage extends Component {
                         </Grid>
                     </form>
                 </Grid>
-            </Grid>            
+            </Grid>
         )
+    }
+
+    setBoardName(e) {
+        this.setState({newBoardName: e.target.value});
+        // e.target.value is the text from our input
     }
 
     onSubmitCreateBoard(e)
@@ -107,6 +185,23 @@ class MainPage extends Component {
 
     onSubmitGoToBoard(boardId){
         this.props.boardPageCallback(boardId)
+    }
+
+    onSetBoardName(boardId){
+        this.setState({newBoardId: boardId.toString()})
+        console.log(this.state.newBoardId + " " + this.state.newBoardName);
+        let that = this
+		axios.post('https://trollo195.herokuapp.com/boards/changeName/' + this.state.newBoardId.toString(),{            
+            name: this.state.newBoardName
+        })
+            .then(function(response){
+                NotificationManager.success(that.state.newBoardName, 'Changed Name Succeed!');
+                that.loadBoardsList()
+                that.setState({newBoardName: ''})
+            })
+            .catch(function(error){
+                NotificationManager.error(error.response.data, 'Change board name faild!')
+            })
     }
 }
 
